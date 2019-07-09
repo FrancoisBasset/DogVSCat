f6598d02d8ecadc43b38a0e65bcb8934e7d002d2
Francois <francois@email.com>
2019-07-09 18:47:38 +0200

    result.py: fichier python pour afficher les resultats

---
+++ result.py
@@ -0,0 +1,70 @@
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.estimator import regression
+import matplotlib.pyplot as plt
+import tflearn
+import os
+import numpy as np
+from test_data import process_test_data
+
+test_data = process_test_data()
+
+fig = plt.figure()
+
+SIZE = 50
+
+convnet = input_data(shape=[None, SIZE, SIZE, 1], name='input')
+
+convnet = conv_2d(convnet, 32, 2, activation='relu')
+convnet = max_pool_2d(convnet, 2)
+
+convnet = conv_2d(convnet, 64, 2, activation='relu')
+convnet = max_pool_2d(convnet, 2)
+
+convnet = conv_2d(convnet, 32, 2, activation='relu')
+convnet = max_pool_2d(convnet, 2)
+
+convnet = conv_2d(convnet, 64, 2, activation='relu')
+convnet = max_pool_2d(convnet, 2)
+
+convnet = conv_2d(convnet, 32, 2, activation='relu')
+convnet = max_pool_2d(convnet, 2)
+
+convnet = conv_2d(convnet, 64, 2, activation='relu')
+convnet = max_pool_2d(convnet, 2)
+
+convnet = fully_connected(convnet, 1024, activation='relu')
+convnet = dropout(convnet, 0.8)
+
+convnet = fully_connected(convnet, 2, activation='softmax')
+convnet = regression(convnet, optimizer='adam', learning_rate=1e-20, loss='categorical_crossentropy', name='targets')
+
+model = tflearn.DNN(convnet, tensorboard_dir='log')
+
+
+if os.path.exists("dogvscat.meta"):
+    model.load("dogvscat")
+    print("model loaded")
+
+for num, data in enumerate(test_data):
+    img_num = data[1]
+    img_data = data[0]
+
+    y = fig.add_subplot(3, 5, num + 1)
+
+    orig = img_data
+    data = img_data.reshape(SIZE, SIZE, 1)
+
+    model_out = model.predict([data])[0]
+
+    if np.argmax(model_out) == 1:
+        str_label = "Dog"
+    else:
+        str_label = "Cat"
+
+    y.imshow(orig, cmap="gray")
+    plt.title(str_label)
+
+    y.axes.get_xaxis().set_visible(False)
+    y.axes.get_yaxis().set_visible(False)
+plt.show()
