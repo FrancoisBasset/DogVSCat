f9e8b1f5ff8a12f6a30e8dddfe4f8c5ce1989b5b
Francois <francois@email.com>
2019-07-09 18:26:41 +0200

    train_model.py: on n'affiche plus les resultats, uniquement pour entrainement

--- train_model.py
+++ train_model.py
@@ -4,17 +4,13 @@ import tflearn
 from tflearn.layers.conv import conv_2d, max_pool_2d
 from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.estimator import regression
-import tflearn.datasets.mnist as mnist
 import tensorflow
-
-from test_data import process_test_data
 
 SIZE = 50
 
 tensorflow.reset_default_graph()
 
 train_data = np.load("train_data.npy", allow_pickle=True)
-test_data = process_test_data()
 
 convnet = input_data(shape=[None, SIZE, SIZE, 1], name='input')
 
@@ -63,31 +59,3 @@ model.fit({'input': X}, {'targets': Y}, n_epoch=1, validation_set=({'input': tes
     snapshot_step=500, show_metric=True, run_id='dogvscat')
 
 model.save("dogvscat")
-
-
-import matplotlib.pyplot as plt
-
-fig = plt.figure()
-
-for num, data in enumerate(test_data):
-    img_num = data[1]
-    img_data = data[0]
-
-    y = fig.add_subplot(3, 5, num + 1)
-
-    orig = img_data
-    data = img_data.reshape(SIZE, SIZE, 1)
-
-    model_out = model.predict([data])[0]
-
-    if np.argmax(model_out) == 1:
-        str_label = "Dog"
-    else:
-        str_label = "Cat"
-
-    y.imshow(orig, cmap="gray")
-    plt.title(str_label)
-
-    y.axes.get_xaxis().set_visible(False)
-    y.axes.get_yaxis().set_visible(False)
-plt.show()
