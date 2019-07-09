57e81330278ab2bacc074d8e48b45caa14ee2b0e
Francois <francois@email.com>
2019-07-09 17:12:42 +0200

    clean train_data.py gitignore

--- train_data.py
+++ train_data.py
@@ -8,27 +8,22 @@ CAT_TRAIN = "./Datasets/Cats/Images/"
 DOG_TRAIN = "./Datasets/Dogs/images/"
 SIZE = 50
 
-def create_train_data():
-    training_data = []
+training_data = []
 
-    for img in tqdm(os.listdir(CAT_TRAIN)):
-        path = os.path.join(CAT_TRAIN, img)
+for img in tqdm(os.listdir(CAT_TRAIN)):
+    path = os.path.join(CAT_TRAIN, img)
+
+    img = cv2.resize(cv2.imread(path, cv2.IMREAD_GRAYSCALE), (SIZE, SIZE))
+    training_data.append([np.array(img), np.array([1, 0])])
+
+
+for folder in tqdm(os.listdir(DOG_TRAIN)):
+    for img in tqdm(os.listdir(DOG_TRAIN + folder)):
+        path = os.path.join(DOG_TRAIN, folder + "/" + img)
 
         img = cv2.resize(cv2.imread(path, cv2.IMREAD_GRAYSCALE), (SIZE, SIZE))
-        training_data.append([np.array(img), np.array([1, 0])])
+        training_data.append([np.array(img), np.array([0, 1])])
 
 
-    for folder in tqdm(os.listdir(DOG_TRAIN)):
-        for img in tqdm(os.listdir(DOG_TRAIN + folder)):
-            path = os.path.join(DOG_TRAIN, folder + "/" + img)
-
-            img = cv2.resize(cv2.imread(path, cv2.IMREAD_GRAYSCALE), (SIZE, SIZE))
-            training_data.append([np.array(img), np.array([0, 1])])
-
-
-    shuffle(training_data)
-    np.save("train_data.npy", training_data)
-    return training_data
-
-if __name__ == "__main__":
-    create_train_data()
+shuffle(training_data)
+np.save("train_data.npy", training_data)
